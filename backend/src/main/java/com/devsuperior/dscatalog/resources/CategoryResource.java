554317b1458eb5f7e7d147d3c104dd4dc0c71ab1
Marcos Shirafuchi <marcosfshirafuchi@gmail.com>
2024-07-31 13:11:38 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.resources;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,8 @@ public class CategoryResource {
 
     @GetMapping
     //O ResponseEntity vai encapsular uma resposta HTTP
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -16,7 +19,18 @@ public class CategoryService {
 
     //Garante a integridade das transações no banco de dados
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = repository.findAll();
+
+        //Cada x(elemento) do list vai ser transformado elemento da listDTO
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+        /*//Outra forma de converter list para listDTO
+        List<CategoryDTO> listDTO = new ArrayList<>();
+        //Cada elemento(cat) da list é colocado no listDTO
+        for (Category cat : list){
+            listDTO.add(new CategoryDTO(cat));
+        }
+        return listDTO;*/
     }
 }
