fad4f38469cb83165fac9725b3fc434a36c4844e
Marcos Shirafuchi <marcosfshirafuchi@gmail.com>
2024-08-01 08:34:54 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,12 +3,14 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,5 +34,13 @@ public class CategoryService {
             listDTO.add(new CategoryDTO(cat));
         }
         return listDTO;*/
+    }
+
+    //Garante a integridade das transações no banco de dados
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
